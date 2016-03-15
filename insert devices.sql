fc5f9d751e95700d6f64736ee5522c9520ac40dd
okvak <vadim-0497@rambler.ru>
2016-03-16 00:20:32 +0200

    Delete insert devices.sql

--- insert devices.sql
+++
@@ -1,8 +0,0 @@
-use prime
- insert into devices values
- ('dell','server',1,'operating','october 29,2010','new->operating','september 23,2011 14:46:32'),
- ('epson','printer',2,'new','november 06,2013','',''),
- ('linksys','router',1,'repairing','may 12,2014','operating->repairing','february 28,2016 15:35:04'),
- ('hp','computer',2,'operating','march 16,2015','repairing->operating','october 05,2015 09:51:22')
-
-select*from devices
