33a15f742a6ac06fad8477a9591d703dc3eee47c
okvak <vadim-0497@rambler.ru>
2016-03-16 00:20:17 +0200

    Delete insert users.sql

--- insert users.sql
+++
@@ -1,7 +0,0 @@
-use prime
-INSERT INTO users VALUES
-('ras1','qwerty','haaakd@google.com','admin'),
-('vas1','ytrewq','nordis@google.com','engineer'),
-('kvas1','qwerty','kaalt@google.com','user'),
-('bras1','ytrewq','kuurz@google.com','user')
-select*from users
