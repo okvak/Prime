a7888c6ea8e80a3d6b1287439579ea2f5aa35818
okvak <vadim-0497@rambler.ru>
2016-03-16 00:20:25 +0200

    Delete insert history.sql

--- insert history.sql
+++
@@ -1,8 +0,0 @@
-use prime
-insert into history values
-('ras1','haaakd@google.com','march 10,2016 12:43'),
-('vas1','nordis@google.com','march 1,2016 16:30'),
-('kvas1','kaalt@google.com','december 6,2015 19:06'),
-('bras1','kuurz@google.com','february 25,2015 11:49')
-
-select*from history
