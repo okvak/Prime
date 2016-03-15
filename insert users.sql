ebfd191a50737d1a80106bfd311f39b7b0c17988
okvak <vadim-0497@rambler.ru>
2016-03-16 00:17:27 +0200

    Added files via upload

---
+++ insert users.sql
@@ -0,0 +1,7 @@
+use prime
+INSERT INTO users VALUES
+('ras1','qwerty','haaakd@google.com','admin'),
+('vas1','ytrewq','nordis@google.com','engineer'),
+('kvas1','qwerty','kaalt@google.com','user'),
+('bras1','ytrewq','kuurz@google.com','user')
+select*from users
